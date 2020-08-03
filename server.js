b68024ac37880a4f55a446ceb0780e41def5e2ff
Ruma Das <rumaadl16@gmail.com>
2020-08-03 12:52:28 +0930

    adding mongo db

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app =express();
+mongoose.connect('mongodb+srv://admin:1234@cluster0-slihp.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser: true})
+.then(()=>console.log("MongoDB Connected"))
+.catch(err => console.error(err));
 
 app.get('/', (req,res) => {
     res.send("Hello World")
 })
+
 
 app.listen(5000);
