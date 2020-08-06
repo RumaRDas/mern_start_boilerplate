8ada90099bb42bbcaa2358e8326e927ab51d6057
Ruma Das <rumaadl16@gmail.com>
2020-08-07 09:14:16 +0930

    adding authentacitin

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -35,21 +36,51 @@ const userSchema = mongoose.Schema({
     }
 })
 
-userSchema.pre('save',(next) =>{
+//for becrypt
+userSchema.pre('save', function(next){
     var user = this;
     if (user.isModified('password')) {
         bcrypt.genSalt(saltRounds, (err, salt) => {
             if (err) return next(err);
-
             bcrypt.hash(user.password, salt, (err, hash) => {
                 if (err) return next(err);
                 user.password = hash
+                next()
             })
         })
-    }else{
+    } else {
         next()
     }
 });
 
-const User = mongoose.model('User', userSchema)
-module.exports = User;
+//for compare pasword
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+        if (err) return cb(err);
+        cb(null, isMatch)
+    })
+}
+
+// for token
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secret')
+    user.token = token;
+    user.save((err, user) => {
+        if (err) return cb(err)
+        cb(null, user);
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb){
+    var user = this;
+    jwt.verify(token, 'secret', function(err, decode){
+        user.findOne({'_id':decode,'token':token}, (err, user) => {
+            if(err) return cb(err)
+            cb(null, user)
+        })
+    })
+
+}
+const User = mongoose.model('User', userSchema);
+module.exports = {User}; 
--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key')
-
-const User = require('./models/user');
+const { auth } = require('./middleware/auth')
+const {User} = require('./models/User');
 
 const app = express();
 mongoose.connect(config.mongoURI, { useNewUrlParser: true })
@@ -15,22 +15,55 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(cookieParser());
 
+
+app.get('/api/user/auth', auth, (req, res) => {
+    res.status(200).json({
+        _id: req._id,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role
+    })
+})
+
 app.post('/api/users/register', (req, res) => {
-    const user = new User(req.body)
-    user.save((err, userData) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
         if (err) return res.json({ success: false, err })
         return res.status(200).json({
-            success:true
+            success: true,
+            userData: doc
+        })
+    })
+})
+
+app.post('/api/user/login', (req, res) => {
+    //find email
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({ loginSuccess: false, message: " Email not Found!" });
+        //password compare 
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: " Wrong Password!" })
+            //generate Token
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie('x_auth', user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true
+                    })
+            })
+
+
         })
     })
 })
 
 app.get('/', (req, res) => {
-    res.json({" Hello ~hhhh": "Hi ~~"})
-})
-
-app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.json({ " Hello ~hhhh": "Hi ~~" })
 })
 
 
